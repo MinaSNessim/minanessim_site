f7282a11e16abbda003ed2a2bbe9663af7f56b08
Mina Nessim <msnessim@gmail.com>
2024-03-19 14:40:06 +0400

    updated logo b4 publish

--- website/settings.py
+++ website/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'website/db.sqlite3',
     }
 }
 
